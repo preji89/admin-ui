0afb00c99012feef77c2f4a4afe76c8732f44a43
Prejita Deepak <122247584+preji89@users.noreply.github.com>
2023-02-28 01:38:49 +0530

    Add. Users Table Layout and Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
+import {Button,Table} from 'react-bootstrap';
 import './App.css';
 
+
 function App() {
+
+  const [users,setUsers]= useState([]);
+
+  useEffect(()=>{
+    fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
+    .then((res)=>res.json())
+    .then((json)=>{
+      setUsers(json);
+
+    });
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Table striped bordered hover size='xl'style={{"table-layout":"auto","width":"100%"}}>
+        <thead><tr>
+          <th><input type="checkbox"></input></th>
+           <th>Name</th>
+           <th>Email</th>
+           <th>Role</th>
+           <th>Actions</th>
+        </tr></thead>
+        <tbody>
+        {users.map((user)=>(
+             <tr key={user.id}>
+            <th><input type="checkbox"></input></th>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.role}</td>
+            <td>
+            <i className="bi bi-pencil-square"/>
+            {"  "}
+            <i className='bi bi-trash' style={{color:"crimson"}}/>
+    
+            </td>
+            </tr>
+
+          ))}
+        </tbody>
+          
+      </Table>
+      <Button variant="danger">Delete Selected</Button>
+      
+    </main>
   );
 }
+
 
 export default App;
